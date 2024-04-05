03a63e6b71b4efbb66e9d701bf73e56de2426b7f
Edward <edkazcarlson@gmail.com>
2024-04-04 22:48:53 -0700

    allow the detection yol to properly pick up on inputch post-transform

--- ultralytics/models/yolo/detect/train.py
+++ ultralytics/models/yolo/detect/train.py
@@ -89,7 +89,7 @@ class DetectionTrainer(BaseTrainer):
 
     def get_model(self, cfg=None, weights=None, verbose=True):
         """Return a YOLO detection model."""
-        model = DetectionModel(cfg, nc=self.data["nc"], verbose=verbose and RANK == -1, backbonePath=self.args.backbonePath)
+        model = DetectionModel(cfg, nc=self.data["nc"], verbose=verbose and RANK == -1, backbonePath=self.args.backbonePath, ch=self.args.inputCh)
         if weights:
             model.load(weights)
         return model
