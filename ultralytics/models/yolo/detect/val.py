6d0faf4fdfefeb3d55c8798c5e7de44df54aa753
Ubuntu <edkazcarlson@gmail.com>
2024-08-19 00:04:55 +0000

    log

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -52,18 +52,17 @@ class DetectionValidator(BaseValidator):
         print(f'start of preprocess batch in val.py, batch dtype is: {batch["img"].dtype}. self.args.half = {self.args.half}')
         batch["img"] = batch["img"].to(self.device, non_blocking=True)
         if self.args.half:
+            print(f'in val.py it self.args.half, before doing .half() {torch.min(batch["img"])}-{torch.max(batch["img"])}')
             batch["img"] = batch["img"].half()
+            print(f'in val.py after doing .half {torch.min(batch["img"])}-{torch.max(batch["img"])}. avg: {torch.mean(batch["img"])}')
         elif (
             batch["img"].dtype == torch.float32
             or batch["img"].dtype == torch.float64
             or batch["img"].dtype == torch.float16
         ):  # already a float
-            print('found a float, doing nothing')
-            # pass
+            pass
         else:
-            print(f'in val.py fall into else statement, doing .float() / 255 {torch.min(batch["img"])}-{torch.max(batch["img"])}')
             batch["img"] = batch["img"].float() / 255.0
-            print(f'in val.py after doing .float() / 255 {torch.min(batch["img"])}-{torch.max(batch["img"])}. avg: {torch.mean(batch["img"])}')
 
         for k in ["batch_idx", "cls", "bboxes"]:
             batch[k] = batch[k].to(self.device)
