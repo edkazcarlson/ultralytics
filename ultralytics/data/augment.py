448a1681e5b78824365d1dfc392f1eca4f7f3d20
Ubuntu <edkazcarlson@gmail.com>
2024-09-08 00:21:44 +0000

    cleaning up albumenation calls

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1832,9 +1832,6 @@ class Albumentations:
                 A.MedianBlur(p=0.01),
                 A.ToGray(p=0.01),
                 A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.0),
-                A.RandomGamma(p=0.0),
-                A.ImageCompression(quality_lower=75, p=0.0),
             ]
 
             # Compose transforms
